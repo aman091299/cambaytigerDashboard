c591eb1700bc2562dbcb7db86ef4d4f70078e5f4
aman <aman091299@gmail.com>
2025-06-14 02:01:04 +0530

    adding products

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,9 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className= "flex"
       >
+        <Sidebar/>
         {children}
       </body>
     </html>
