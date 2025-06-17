6a46afc6a0245a573d690ccc431277ce302328a0
aman <aman091299@gmail.com>
2025-06-17 19:08:43 +0530

    ading

--- app/components/Logout.jsx
+++ app/components/Logout.jsx
@@ -1,31 +1,34 @@
 'use client'
 import { useDispatch } from 'react-redux';
 import { removeUser } from '../store/userSlice';
+import { BASE_URL } from '../utils/constants';
+import axios from 'axios';
+import {useRouter} from 'next/navigation';
 
 const Logout = () => {
     const dispatch=useDispatch();
+    const router=useRouter();
 
     const logoutHandler = async () => {
+      console.log("inside logout")
     try {
       const res = await axios.post(
         BASE_URL + "/logout",{},{
           withCredentials:true,
         }
       );
+      console.log("res",res)
       dispatch(removeUser());
        router.push("/");
       
     } catch (error) {
-<<<<<<< HEAD
-=======
-      
->>>>>>> 2f5a3cf (adding yfyfy f fhj)
+      console.log("error",error)
       return console.log("Error" + error.message);
     }
   };
 
   return (
-     <button className="btn flex justify-end " onClick={()=>logoutHandler}>Logout</button>  )
+     <button className="btn flex justify-end " onClick={()=>logoutHandler()}>Logout</button>  )
     }
 
 export default Logout
